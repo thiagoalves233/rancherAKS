fcc4bc329da6a969b2fdcaf6e190462437a2fffe
thiagoalves233 <45635763+thiagoalves233@users.noreply.github.com>
2024-09-08 12:22:16 -0300

    Update credentials.sh

--- credentials.sh
+++ credentials.sh
@@ -1,4 +1,4 @@
-export ARM_TENANT_ID="6849eec3-5c1d-4629-acce-eadec66cc18c"
-export ARM_CLIENT_ID="84d28679-2f09-4464-800a-93d34fde3dda"
-export ARM_CLIENT_SECRET="D4D.kslmwNMUNIkf2U6MyupRqLIwi-RVZj"
-export ARM_TENANT_ID="77f91f97-eeac-4ec0-bff5-a33822b0a029"
+export ARM_TENANT_ID="XXXX"
+export ARM_CLIENT_ID="XXXX"
+export ARM_CLIENT_SECRET="XXXX"
+export ARM_TENANT_ID="XXXX"
